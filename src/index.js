b405ba1fceb8c15ac0cab01884da606831e0c181
Dinesh yadav <dineshyadav74950@gmail.com>
2024-04-30 22:55:50 +0530

    In this project i understood the without react dom how to render the content

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-ReactDOM.render(<h1>ye rista kya kahalata hai</h1>,document.getElementById('root'));
+const arr = [];
+// in react v16 it's possible for render() to return undefined an array of the elements
+ReactDOM.render(
+  [
+<h1>ye rista kya kahalata hai</h1>,
+<p>hey this is paragraph </p> ,
+<h2>please subscribe my channel</h2>
+],document.getElementById('root'));
 
 var h1 = document.createElement('h1');
 h1.innerHTML = 'Hello Dinesh';
