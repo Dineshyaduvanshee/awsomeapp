73123e064a8d1ecf93afbd0b33d2e6ce30388b81
Dinesh yadav <dineshyadav74950@gmail.com>
2024-04-30 22:28:46 +0530

    This is my new awsome project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(<h1>ye rista kya kahalata hai</h1>,document.getElementById('root'));
+
+var h1 = document.createElement('h1');
+h1.innerHTML = 'Hello Dinesh';
+document.getElementById('root').appendChild(h1);
