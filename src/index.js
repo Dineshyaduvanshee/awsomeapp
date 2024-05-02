1e9b6cd868dcba3e3da5757aea900866519dabc5
Dinesh yadav <dineshyadav74950@gmail.com>
2024-05-02 22:20:48 +0530

    In this is project i have add react fragment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-const arr = [];
-// in react v16 it's possible for render() to return undefined an array of the elements
-ReactDOM.render(
-  [
-<h1>ye rista kya kahalata hai</h1>,
-<p>hey this is paragraph </p> ,
-<h2>please subscribe my channel</h2>
-],document.getElementById('root'));
+// const arr = [];
+// // in react v16 it's possible for render() to return undefined an array of the elements
+// ReactDOM.render(
+//   [
+// <h1>ye rista kya kahalata hai</h1>,
+// <p>hey this is paragraph </p> ,
+// <h2>please subscribe my channel</h2>
+// ],document.getElementById('root'));
 
-var h1 = document.createElement('h1');
-h1.innerHTML = 'Hello Dinesh';
-document.getElementById('root').appendChild(h1);
+// var h1 = document.createElement('h1');
+// h1.innerHTML = 'Hello Dinesh';
+// document.getElementById('root').appendChild(h1);
+
+ReactDOM.render(
+  <React.Fragment>
+    <h1> Thapa Technical </h1>
+    <p>please like and share to all</p>
+    <h2>please subscribe my channel</h2>
+
+  </React.Fragment>,
+  document.getElementById('root')
+);
